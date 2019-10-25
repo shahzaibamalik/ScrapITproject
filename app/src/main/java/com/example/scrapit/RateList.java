20b7e0436f3b70d0a33b8fcc73e86bcb43876608
shahzaibamalik <shahzaibamalik@gmail.com>
2019-10-25 05:36:14 -0700

    Add files

---
+++ app/src/main/java/com/example/scrapit/RateList.java
@@ -0,0 +1,14 @@
+package com.example.scrapit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RateList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate_list);
+    }
+}
